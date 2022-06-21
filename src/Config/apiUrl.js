6a532aebddae6a28fe425a652be8118c9bf340eb
cesarjacome97 <107264076+cesarjacome97@users.noreply.github.com>
2022-06-21 08:39:10 -0500

    Add files via upload
    Changed the api key

--- src/Config/apiUrl.js
+++ src/Config/apiUrl.js
@@ -1,7 +1,7 @@
 export const authApi = 'http://192.168.50.91:9200';
 
-export const backendAPI = 'http://192.168.50.78:9090';
+export const backendAPI = 'http://localhost:9090';
 
 export const googleApi = 'https://maps.googleapis.com/maps';
 
-export const googleMapsApiKey = 'AIzaSyDoLzHyM8TTL2LnInXa18HrrrdY2gG4CbE';
+export const googleMapsApiKey = 'kb0eyz4s.live.rnum.com';
